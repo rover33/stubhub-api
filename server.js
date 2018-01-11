bfbf65974771795b70ddc8feb8b0b6713d5e1459
rover33 <remy.j.pearlstone@gmail.com>
2018-01-11 12:15:58 -0700

    wellll

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ let express = require('express'),
     app = express(),
     request = require('request'),
     axios = require('axios');
-    dotenv = require('dotenv').config({path: './model/env'})
 
 //parse incoming form data
 //populate the req.body object
@@ -12,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //tokens
-let headers = require("./models/env")
+let headers = require('dotenv').config({path: './model/env'})
 let db = require('./models')
 
 // serve static files in public
