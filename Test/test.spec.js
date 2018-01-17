d0f6861f59bfeef57ed76c6e25475740bc5a5e80
rover33 <remy.j.pearlstone@gmail.com>
2018-01-17 08:58:54 -0700

    mocha testing

--- Test/test.spec.js
+++ Test/test.spec.js
@@ -3,7 +3,7 @@ let Festival = require('../models/festival'),
     expect = require('chai').expect
     request = require('request')
     
-
+//new festival tests
 describe('checking if festival model works', function(){
     describe('new', function(){
         it('initializes a new festival', function(){
@@ -11,7 +11,22 @@ describe('checking if festival model works', function(){
             festival.name = "Coachella"
             expect(festival.name).to.equal('Coachella');
         });
-    });
+        it("creates a new city", function(){
+            var festival = new Festival();
+            festival.city = "palm springs"
+            expect(festival.city).to.equal("palm springs")
+        });
+        it("creates a new venue", function(){
+            var festival = new Festival();
+            festival.venue = "Empire Polo Fields"
+            expect(festival.venue).to.equal("Empire Polo Fields")
+        })
+        it("creates a new date", function(){
+            var festival = new Festival();
+            festival.date = "April 2018"
+            expect(festival.date).to.equal("April 2018")
+         });
+    })
 
     describe('new User', function(){
         it('initializes a new User', function(){
@@ -19,6 +34,17 @@ describe('checking if festival model works', function(){
             myUser.userName = "Remy"
             expect(myUser.userName).to.equal('Remy');
         });
+        it('initializes a new email', function(){
+            var myUser = new User();
+            myUser.email = "remy.j.pearlstone@gmail.com"
+            expect(myUser.email).to.equal('remy.j.pearlstone@gmail.com');
+        });
+        it('creates a new pwd', function(){
+            var myUser = new User();
+            myUser.password= "12345"
+            expect(myUser.password).to.equal('12345');
+        });
+
     });
 
 });
